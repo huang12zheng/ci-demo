78f77818fb01c17bcb7194f15acb7bb77505e56b
huang12zheng <805104533@qq.com>
2022-01-06 10:55:32 +0800

    v21

--- ci/scripts/get_dep.sh
+++ ci/scripts/get_dep.sh
@@ -4,7 +4,8 @@ set -x
 git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=`pwd`/depot_tools:"$PATH"
 gclient
-fetch webrtc --no-history --nohooks
+ls
+fetch --no-history --nohooks webrtc
 
 # if [ "`uname -s`" != "Darwin" ]; then
 # depot_tools/build/install-build-deps.sh
