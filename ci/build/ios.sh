00144466566fbfbb255de9b23e3d628fc9b41d44
huang12zheng <805104533@qq.com>
2022-01-06 17:55:54 +0800

    v32

--- ci/build/ios.sh
+++ ci/build/ios.sh
@@ -1,4 +1,5 @@
-python ./tools_webrtc/ios/build_ios_libs.py --build_config release --arch device:arm64 simulator:arm64 simulator:x64 --extra-gn-args \ 
+echo target_os = [ 'ios' ]>>../../.gclient
+python3 ./tools_webrtc/ios/build_ios_libs.py --build_config release --arch device:arm64 simulator:arm64 simulator:x64 --extra-gn-args \ 
       rtc_libvpx_build_vp9=true \
       rtc_include_tests=false \
       rtc_build_examples=false \
