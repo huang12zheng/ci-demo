95370548243f7f591f96cc02ffcba777309db8e2
huang12zheng <805104533@qq.com>
2022-01-06 09:03:26 +0800

    v16

--- ci/scripts/get_dep.sh
+++ ci/scripts/get_dep.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -ex
+
 git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
 depot_tools/fetch --no-history --nohooks --with_branch_heads webrtc
 depot_tools/gclient sync --no-history --nohooks
